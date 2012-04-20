bc56d9f8418dd5384b60a1d08b5155e39d62382d
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-04-20 11:00:49 +0200

    Modifs et clean.

--- first_try.pl
+++ first_try.pl
@@ -17,7 +17,7 @@ my $host = 'irc.freenode.net';
 my $chan = '#spi2011';
 my $nick = 'teuse';
 
-my $msg_presentation = "Salut, je m'appelle teuse. En fait, je suis son petit frère, teuse est morte. Elle était en Python et je suis en Perl ;)";
+my $msg_presentation = "Salut, je m'appelle teuse. En fait, je suis sa petite soeur, teuse est morte. Elle était en Python et je suis en Perl ;)";
 
 my $path = "$irc_dir/$host/$chan";
 my $path_out = "$path/in"; 
@@ -54,38 +54,56 @@ my @politics = (
     'aubry',
     'réforme',
     'poutou',
-    'npa',
+    'NPA',
     'ump', # ps peut être utilisé pour post-scriptum
-    'bayrou'
+    'bayrou',
+    'eva joly',
+    'nathalie arthaud',
+    'communiste'
 );
 
 my $politics_re = join "|", @politics;
 my %actions = (
     #qr/[^<]*<([^>]*)>.*/ => sub { say $1},
-    qr/[^<]*<([^>]*)>[^$nick\s?:?].*\s((la|le|une?)\s+[^\s]*)\S.*$/ => sub {
+    qr/[^<]*<([^>]*)>[^$nick\s?:?].*\s((la|le|une?)\s+[^\s]*)\s?.*$/ => sub {
         my $str = $2;
         $str =~ s/une /la /;
         $str =~ s/un /le /;
         if (defined $1) { IRCsend("$1: c'est ta mère $str!"); }
         else {IRCsend("c'est ta mère $str!");}
     },
+
+    # Politic must die
     $politics_re => sub { IRCsend(`python ./plugins/insulte.py`)},
-    qr/[^<]*<([^>]*)>\s*$nick\s?:?\s*à?a? boire\s*!?$/ => sub { IRCsend($1." : ".`python ./plugins/choix_boisson.py`);},
+
+    # A boire !
+#    qr/[^<]*<([^>]*)>\s*$nick\s?:?\s*à?a? boire\s*!?$/ => sub { IRCsend($1." : ".`python ./plugins/choix_boisson.py`);},
+
+    # Rappel Phrase
     qr/[^<]*<([^>]*)>\s*$nick\s?:?\s*retiens\s+(.*)$/ => sub { my $reponse = `./plugins/rappelPhrase.py souvenir $1 $2`; say $reponse; IRCsend($reponse);},
     qr/[^<]*<([^>]*)>\s*$nick\s?:?\s*(tell me more)\s*$/ => sub { IRCsend(" ".`python ./plugins/rappelPhrase.py rappel $1 $2`);},
     qr/[^<]*<([^>]*)>\s*$nick\s?:?\s*(oublie tout)\s*$/ => sub { IRCsend(" ".`python ./plugins/rappelPhrase.py oublis $1 $2`);},
+
+
+    # Rot13
     qr/[^>]*$nick\s?:?\s*rot13 (.*)$/ => sub {
         my $msg = $1;
         $msg =~ s/\'/#/g;
         $msg = `echo $msg| tr 'A-Za-z' 'N-ZA-Mn-za-m'`;
         $msg =~ s/#/\'/g; IRCsend($msg);
     },
-	qr/[^>]*$nick:? casse toi.*/ => sub { $in->close(); exit 0;},
+
+    # Reboot & Cie
+	qr/[^<]*@?matael>\s*$nick:? casse toi.*/ => sub { $in->close(); exit 1;},
+	qr/[^<]*@?matael>\s*$nick:? reboot now.*/ => sub { $in->close(); system("kill -9 $$; perl first_try.pl");},
+    
+    # Autres
     qr/[^>]*.*(yop?|bouga|morning|ahoy|plop)\s.*/ => sub { my $i = rand @yops; IRCsend($yops[$i]); },
     qr/[^>]*[^$nick\s*:?].*$nick.*/ => sub { my $i = rand @meh; IRCsend($meh[$i]); },
 #   qr/[^>]*[^$nick\s*:?][^(?à boire|rot13)]*$nick [^(?à boire|rot13)]*/ => sub { my $i = rand @meh; IRCsend($meh[$i]); },
 	qr/[^>]*$nick:?.* qui es tu.*/ => sub { IRCsend($msg_presentation);},
 	qr/[^>]*.*cookie.*/ => sub { IRCsend('Owi ! \o/');},
+	qr/[^>]*.*paste\s.*/ => sub { IRCsend('http://pastebin.archlinux.fr/');},
     qr/[^>]* \[Exo::([^\]]*)\].*/ => sub { IRCsend("Check http://exos.matael.org/?n=$1"); },
     qr/[^>]*.*\[~([^\/]*)\/([^\]]*)\].*/ => sub { IRCsend("Check http://matael.org/~$1/$2"); },
     qr/[^>]*.*pelle.*/ => sub { IRCsend("teuse");}
