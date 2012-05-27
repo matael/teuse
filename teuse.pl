06be86c40fbde8a94f0a1d858cb98e2d18b31702
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-05-27 23:27:29 +0200

    Ajout du début de teuse (non-fonctionnel) pour synchro

---
+++ teuse.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+# Simple IRC bot
+
+use strict;
+use warnings;
+use 5.010;
+use Bot::BasicBot;
+
+package Teuse;
+use base qw( Bot::BasicBot );
+
+
+sub said {
+    my ($self, $a) = @_;
+
+    # offend if in PM
+    if ($a->{'address'} eq 'msg') {
+       $self->say(
+           who=>$a->{'who'},
+           channel=>'msg', # answer privately
+           body=>"FUCK OFF ! Parle en public si t'es un homme"
+       );
+    }
+}
+
+sub emoted {
+    my ($self, $a) = @_;
+
+    if ($a->{msg} =~ m/.*à|a soif.*/){
+        $self->say(
+            who=>$a->{who},
+            channel => $a->{channel},
+            body=>`python ./plugins/choix_boisson.py`
+        );
+    }
+}
+
+1;
+
+my $teuse = Teuse->new(
+    server => "irc.freenode.org",
+    channels => ["#spi2011"],
+    nick => 'teuse',
+)->run();
